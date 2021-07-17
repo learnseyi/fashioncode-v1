be9740f0756d27a2c0379b8951fd7ea266f2c5a4
Seyi Agboola <learnseyi@gmail.com>
2021-07-17 12:56:02 -0500

    added user credentials and updatedright offset value on mobile display'

--- src/Components/Credentials/Credentials.js
+++ src/Components/Credentials/Credentials.js
@@ -52,10 +52,10 @@ useEffect(()=>{
         <MDBCol className="credentials-container"  
         style={detailsStyle} >
         <MDBBtn className='credentials-btn' 
-        color="light-green"
-        onClick={()=>setRight(-17 + 'em')} 
+        
+        onClick={()=>setRight(-8.5 + 'em')} 
         href="#">Login Credentials</MDBBtn>
-        <MDBCard className = 'users'style={{ width: "40vw" ,background:"yellow",textAlign:"center"}}>
+        <MDBCard className = 'users'style={{ width: "60vw" ,background:"yellow",textAlign:"center"}}>
         <MDBBtn 
         className='position-absolute top-0 reset-btn' 
         outline 
@@ -67,12 +67,12 @@ useEffect(()=>{
           <MDBCardBody>
             <MDBCardTitle >
                 <MDBContainer >
-                    <MDBRow className="justify-content-center flex-column" style={{width: '14em'}}>
+                    <MDBRow className="justify-content-center flex-column" style={{width: '18em'}} >
                     <h6 className='text-center mt-4'>LOGIN CREDENTIALS</h6>
                     <MDBCardText className="mb-0">
                     <p>Please use credentials below to access restricted area</p>
                     </MDBCardText>
-                    <MDBCol>
+                    <MDBCol className='d-flex justify-content-lg-center'>
                     <MDBCardText className='text-left user-info'>
                         <p>USER ONE:</p>
                         <p> Name: &emsp;&emsp;&emsp;John Doe </p>
@@ -83,17 +83,25 @@ useEffect(()=>{
                     <MDBCol className='mt-3'>
                     <MDBCardText className='text-left user-info'>
                         <p>USER TWO:</p>
-                        <p> Name: &emsp;&emsp;&emsp;John Doe </p>
-                        <p>Username: &emsp; Jdoe</p>
-                        <p>password: &emsp;&nbsp;1111</p>
+                        <p> Name: &emsp;&emsp;&emsp;Jessica Davis </p>
+                        <p>Username: &emsp;  jdavis</p>
+                        <p>password: &emsp;&nbsp;2222</p>
                         </MDBCardText>
                     </MDBCol>
-                    <MDBCol className='mt-3'>
+                    <MDBCol className='mt-3 d-flex justify-content-lg-center'>
                     <MDBCardText className='text-left user-info'>
                         <p>USER THREE:</p>
-                        <p> Name: &emsp;&emsp;&emsp;John Doe </p>
-                        <p>Username: &emsp; Jdoe</p>
-                        <p>password: &emsp;&nbsp;1111</p>
+                        <p> Name: &emsp;&emsp;&emsp;Steven Williams </p>
+                        <p>Username: &emsp; swilliams</p>
+                        <p>password: &emsp;&nbsp;3333</p>
+                        </MDBCardText>
+                    </MDBCol>
+                    <MDBCol className='mt-3 '>
+                    <MDBCardText className='text-left user-info'>
+                        <p>USER FOUR:</p>
+                        <p> Name: &emsp;&emsp;&emsp;Sarah Smith </p>
+                        <p>Username: &emsp; ssmith</p>
+                        <p>password: &emsp;&nbsp;4444</p>
                         </MDBCardText>
                     </MDBCol>
                     </MDBRow>
